b05007715ed946f93d96058ba314d422271e0256
SukanyaT2022 <sukanyat2022a@gmail.com>
2024-12-13 12:13:33 -0500

    add

--- src/component/MyDec9todo.tsx
+++ src/component/MyDec9todo.tsx
@@ -14,22 +14,27 @@ const MyDec9todo = () => {
 const [inputItem, setInputItem] = useState('')
 const [allItems, setAllItems] = useState<string[]>([])
 const [colorStar, setColorStar] = useState('')
+const [searchItems, setSearchItems] = useState('')
 
 const starFunc=(item:string)=>{
-  setColorStar(item)
+setColorStar(item)
 }
 
 const addFunc =()=>{
-  setAllItems([...allItems,inputItem])
+setAllItems([...allItems,inputItem])
 setInputItem('')
 }
 const removeFunc =(removeTask:string)=>{
-  let updateItems = allItems.filter((item)=>item !== removeTask )
-  setAllItems(updateItems)
+let updateItems = allItems.filter((item)=>item !== removeTask )
+setAllItems(updateItems)
+}
+const searchTextFunc=(searchItems:string)=>{
+setAllItems(allItems.filter((item)=>item == searchItems))
+setSearchItems('')
 }
 
   return (
-    <View style={{flex:1,}}>
+    <View style={{flex:1,  position:'relative'}}>
     <View style={{flexDirection:'row'}}>
     <TextInput 
     style={{borderColor:'green', borderWidth:5, padding:10, width:'80%'}} 
@@ -54,18 +59,23 @@ renderItem={({item})=>(
 
 <Icon name="staro" size={20} color={colorStar == item? "red": "green"}  
 onPress={()=>starFunc(item)}/>
-
   <Text>{item}</Text>
-
 <TouchableOpacity onPress={()=>removeFunc(item)}><Text>Remove</Text></TouchableOpacity>
   </View>
-
 )
 }
 />
 </View>
 {/* end flatlist */}
+
+{/* search box */}
+<View style={{flex:1, flexDirection:'row',alignItems:'center', justifyContent: 'space-between',borderColor:'orange', borderWidth:2, padding:10, marginVertical:15, position:'absolute', bottom:0, left:20, right:20}}>
+<TextInput style={{}} onChangeText={(text)=>setSearchItems(text)} value={searchItems} placeholder='Search Task'/>
+<TouchableOpacity onPress={()=>searchTextFunc(searchItems)} ><Text>Search</Text></TouchableOpacity>
+</View>
+
     </View>
+
   )
 }
 
